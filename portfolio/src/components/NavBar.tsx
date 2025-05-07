120d34b181ca595249e295a8b4d15d013dfd65b8
cesargarciari <cesarolandogr@gmail.com>
2025-05-06 23:25:48 -0600

    Initial set up done

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -5,13 +5,10 @@ const NavBar = () => {
     <nav className='flex shadow justify-around items-center bg-black py-3 fixed top-0 w-full'>
       
       <span className='flex items-center'>
-        <img src="/logo-round.png" alt="Logo" className='w-12 h-12 rounded-full' />
-        <span className='text-white text-2xl font-semibold ml-2'>
-            César García
-        </span>
+        <Link to="/" className='text-white text-2xl font-semibold ml-2'>César García</Link>  
       </span>
       
-      <Link to="/" className='text-white'>Home</Link>  
+      
       <Link to="/about" className='text-white'>About</Link>
       <Link to="/projects" className='text-white'>Projects</Link>
       <Link to="/experience" className='text-white'>Experience</Link>
--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-slate-50 w-full flex items-center justify-center'>
+    <div className='min-h-screen w-full flex items-center justify-center'>
         <div>
             Home
         </div>
