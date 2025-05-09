d405fa80ff63f0966a2b4c5530a32c0c9dd96b6a
cesargarciari <cesarolandogr@gmail.com>
2025-05-08 22:20:56 -0600

    NavBar finished for now

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -2,18 +2,23 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <nav className='flex shadow justify-around items-center bg-black py-3 fixed top-0 w-full'>
+    <header className=''>
+    
+    <nav className='navbar'>
+
+    <div className='absolute'>
+    <Link to="/" className='text-white font-semibold ml-2 px-50'>César García</Link>  
+    </div>
       
-      <span className='flex items-center'>
-        <Link to="/" className='text-white text-2xl font-semibold ml-2'>César García</Link>  
-      </span>
-      
-      
-      <Link to="/about" className='text-white'>About</Link>
-      <Link to="/projects" className='text-white'>Projects</Link>
-      <Link to="/experience" className='text-white'>Experience</Link>
-      <Link to="/contact" className='text-white'>Contact Me</Link>
+    <div className="mx-auto flex items-center justify-between gap-10">
+        <Link to="/about" className="text-white">About</Link>
+        <Link to="/projects" className="text-white">Projects</Link>
+        <Link to="/experience" className="text-white">Experience</Link>
+        <Link to="/contact" className="text-white">Contact Me</Link>
+    </div>
+ 
     </nav>
+    </header>
   )
 }
 
