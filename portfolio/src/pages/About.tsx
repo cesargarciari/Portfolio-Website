249ee9969f34a7209ab4c091b45497e40f186ef6
cesargarciari <cesarolandogr@gmail.com>
2025-05-08 22:23:13 -0600

    Unfinished pages

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div className='text-white text-9xl'>About</div>
-  )
-}
-
-export default About
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+  const About = () => {
+    return (
+      <div className="justify-center items-center flex flex-col">
+        <Card className="w-128 shadow-amber-100 hover:shadow-lg transition-shadow duration-300">
+          <CardHeader className="text-white">
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+    )
+  }
+  
+  export default About
+  
--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center'>
-        <div>
-            Home
-        </div>
+    <div className='text-white'>
+        Home for now
         </div>
   )
 }
