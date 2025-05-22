d96dee441923c1697f9bfd594179e130845186e9
cesargarciari <cesarolandogr@gmail.com>
2025-05-21 22:32:59 -0600

    Contact page set up

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -1,6 +1,25 @@
+import { cn } from "@/lib/utils"
+
 const Contact = () => {
+
+    
   return (
-    <div>Contact</div>
+    <>
+    <div className="px-4 sm:px-6 md:px-16 lg:px-32 max-w-5xl mx-auto mt-12 sm:mt-16 w-full flex flex-col gap-12">
+
+    
+    <div
+          className={cn(
+            "relative rounded-xl border border-white/20 bg-primary p-6 backdrop-blur-sm"
+          )}
+        >
+            <h3 className="mb-2 text-xl font-semibold text-white">Contact me</h3>
+        </div>
+
+        </div>
+
+        </>
+        
   )
 }
 
