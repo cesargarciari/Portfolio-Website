7428dcdbd737d8d6dd3c096bfc976051d6579318
cesargarciari <cesarolandogr@gmail.com>
2026-02-03 22:00:54 -0700

    fix(ux): reset scroll position when navigating between pages

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import Projects from "./pages/Projects"
 import Experience from "./pages/Experience"
@@ -7,10 +7,20 @@ import Home from "./pages/Home"
 import Footer from "./components/Footer"
 import NotFound from "./pages/404"
 import { ThemeProvider } from "./components/theme-provider"
+import { useEffect } from "react"
 
 const App = () => {
+
+  function ScrollToTopOnRouteChange() {
+    const { pathname } = useLocation();
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  return null;
+}
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ScrollToTopOnRouteChange />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
